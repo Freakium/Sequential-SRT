5c54642d15a4f8a4fa0d8c71240bdc09dfaff89c
Freakium <ross_chan2001@yahoo.ca>
2017-07-28 15:59:23 -0600

    Adding Project

---
+++ SequentialSRT/SequentialSRT.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SequentialSRT
+{
+    partial class frmSequentialSRT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.TBOpen = new System.Windows.Forms.TextBox();
+            this.btnBegin = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 20);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open SRT";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // TBOpen
+            // 
+            this.TBOpen.Location = new System.Drawing.Point(93, 12);
+            this.TBOpen.Name = "TBOpen";
+            this.TBOpen.Size = new System.Drawing.Size(223, 20);
+            this.TBOpen.TabIndex = 1;
+            // 
+            // btnBegin
+            // 
+            this.btnBegin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBegin.Location = new System.Drawing.Point(93, 38);
+            this.btnBegin.Name = "btnBegin";
+            this.btnBegin.Size = new System.Drawing.Size(143, 37);
+            this.btnBegin.TabIndex = 2;
+            this.btnBegin.Text = "Begin";
+            this.btnBegin.UseVisualStyleBackColor = true;
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(160)))), ((int)(((byte)(240)))));
+            this.lblStatus.Location = new System.Drawing.Point(12, 81);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(304, 23);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmSequentialSRT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(328, 113);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnBegin);
+            this.Controls.Add(this.TBOpen);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "frmSequentialSRT";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sequential SRT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox TBOpen;
+        private System.Windows.Forms.Button btnBegin;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ SequentialSRT/SequentialSRT.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SequentialSRT
+{
+    public partial class frmSequentialSRT : Form
+    {
+        public frmSequentialSRT()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Open SRT";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            openFileDialog1.DefaultExt = "srt";
+            openFileDialog1.Filter = "SRT Files (*.srt)|*.srt|All Files (*.*)|*.*";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                TBOpen.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnBegin_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Working...";
+
+            lblStatus.Text = numberAssigner(TBOpen.Text);
+        }
+
+        private string numberAssigner(string filename)
+        {
+            string readIn;
+            int number;
+            int count = 1;
+            bool nextSub = true;
+            string directoryPath = Path.GetDirectoryName(filename);
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filename))
+                {
+                    StreamWriter writer = new StreamWriter(directoryPath + "\\NEWmerated.srt");
+
+                    while (reader.Peek() >= 0)
+                    {
+                        readIn = reader.ReadLine();
+
+                        if (nextSub && Int32.TryParse(readIn, out number))
+                        {
+                            nextSub = false;
+                            writer.WriteLine(count);
+                            count++;
+                        }
+                        else if (readIn == "")
+                        {
+                            nextSub = true;
+                            writer.WriteLine();
+                        }
+                        else
+                            writer.WriteLine(readIn);
+                    }
+
+                    writer.Close();
+                }
+
+                return "SRT Numeration Complete";
+            }
+            catch (Exception)
+            {
+                return "File Does Not Exist";
+            }
+        }
+    }
+}
